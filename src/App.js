bd8198901b1e63ad802a60498785b6107c9ac331
Ifeoma Imoh <imoh_ifeoma@yahoo.co.uk>
2022-03-28 20:03:41 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Card, Col, Form, Input, message } from 'antd';
+import { useState } from 'react';
+import ImageSelector from './components/ImageSelector';
+
+import { uploadImage } from '../src/utility/api';
 
 function App() {
+  const [image, setImage] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
+
+  const formItemLayout = {
+    labelCol: {
+      sm: { span: 4 },
+    },
+    wrapperCol: {
+      sm: { span: 18 },
+    },
+  };
+
+  const onFailedSubmission = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  const onFinish = (values) => {
+    if (image === null) {
+      message.error('You need to upload an image first');
+    } else {
+      setIsUploading(true);
+      uploadImage({
+        file: image,
+        successCallback: (data) => {
+          setIsUploading(false);
+          console.log(data);
+          message.success('Image uploaded successfully.');
+        },
+      });
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ margin: '1%' }}>
+      <Card style={{ margin: 'auto', width: '50%' }}>
+        <Form
+          {...formItemLayout}
+          onFinish={onFinish}
+          onFinishFailed={onFailedSubmission}
+          autoComplete="off"
         >
-          Learn React
-        </a>
-      </header>
+          <Col span={8} offset={9} style={{ marginBottom: '10px' }}>
+            <ImageSelector setImage={setImage} />
+          </Col>
+          <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
+            <Button type="primary" htmlType="submit" loading={isUploading}>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   );
 }
